30e3f64948dcb5847b6ac5c90251f462cc0e21c4
Animesh kumbhakar <75767221+Animeshk123@users.noreply.github.com>
2022-07-09 17:34:04 +0530

    Update Grid.js

--- src/Grid.js
+++ src/Grid.js
@@ -25,9 +25,9 @@ const Grid = (props) => {
           <div className="mt-4 w-screen py-4 overflow-hidden">
             <div className="container mx-auto px-4 myclass">
               {
-                (data.length > 0) ? (data.map((item,index)=> {
+                data.map((item,index)=> {
                     return <Card id={index} date={item.publishedAt} name={item.source.name} url={item.url} title={item.title} img={item.urlToImage} description={item.description}/>
-                })) : <div><Load/><Load/><Load/></div>
+                })
               }
             </div>
           </div>
