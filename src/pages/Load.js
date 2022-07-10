0362bb5e0d9151bbd9fefee0deab53239a782fa7
Animesh kumbhakar <75767221+Animeshk123@users.noreply.github.com>
2022-07-10 08:25:38 +0530

    Update Load.js

--- src/pages/Load.js
+++ src/pages/Load.js
@@ -1,5 +1,5 @@
 const Load = () => {
-    return <div className="shadow-lg bg-white rounded-md p-3 mb-4">
+    return <div className="w-full max-w-sm shadow-lg bg-white rounded-md p-3 mb-4">
              <div className='bg-gray-500 w-full h-44 object-cover object-center rounded-sm'></div>
              <h1 className="bg-gray-500 font-semibold py-3 capitalize text-xl my-2 rounded-sm"></h1>
              <p className='capitalize rounded-sm bg-gray-500 py-1 mt-2'></p>
