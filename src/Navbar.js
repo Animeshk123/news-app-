43f9b92dc669d654f3bdb015718b25f4d66ecdbb
Animesh kumbhakar <75767221+Animeshk123@users.noreply.github.com>
2022-07-20 09:12:53 +0530

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,14 @@
+
+import {NavLink} from "react-router-dom";
+
 const Navbar = () => {
     return(
         <>
           <div className="w-screen py-4 h-auto bg-blue-600 shadow-lg">
             <div className="px-4 container mx-auto">
+              <NavLink to="/">
               <p className="text-white text-xl capitalize">News ApI</p>
+              </NavLink>
             </div>
           </div>
         </>
